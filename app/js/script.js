f704a9c8dcf4c8e854c861da1e187cf334ad399a
hramtsov13 <hramtsov13@mail.ru>
2020-08-31 19:57:52 +0500

    native js tabs

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,26 @@
-//Swup - ajax init
-const swup = new Swup();
+let tab = function () {
+  let tabNav = document.querySelectorAll(".tabs-nav__item");
+  let tabContent = document.querySelectorAll(".tab");
+  let tabName;
+
+  tabNav.forEach((item) => {
+    item.addEventListener("click", selectTabNav);
+  });
+
+  function selectTabNav() {
+    tabNav.forEach((item) => {
+      item.classList.remove("is-active");
+    });
+    this.classList.add("is-active");
+    tabName = this.getAttribute("data-tab-name");
+    selectTabContent(tabName);
+  }
+
+  function selectTabContent(tabName) {
+    tabContent.forEach((item) => {
+      item.classList.contains(tabName) ? item.classList.add("is-active") : item.classList.remove("is-active");
+    });
+  }
+};
+
+tab();
