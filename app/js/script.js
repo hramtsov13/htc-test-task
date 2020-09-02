40f2cf27b2d3b1ee0ed3f6bb42cf3f7df930503e
hramtsov13 <hramtsov13@mail.ru>
2020-09-02 23:54:03 +0500

    login/logout user on JS

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
-let tab = function () {
-  let tabNav = document.querySelectorAll(".nav__item");
-  let tabContent = document.querySelectorAll(".tab");
+function tab() {
+  const tabNav = document.querySelectorAll(".nav__item");
+  const tabContent = document.querySelectorAll(".tab");
   let tabName;
 
   tabNav.forEach((item) => {
@@ -21,7 +21,7 @@ let tab = function () {
       item.classList.contains(tabName) ? item.classList.add("is-active") : item.classList.remove("is-active");
     });
   }
-};
+}
 
 tab();
 
@@ -29,27 +29,69 @@ tab();
 
 let modal = document.getElementById("modal-login");
 let btn = document.getElementById("login-button");
-let close = document.getElementById("login-form");
+
+function closeModal() {
+  modal.style.display = "none"; //close by click in any place
+  document.querySelector("body").style.overflow = "auto"; //page scroll
+}
 
 btn.addEventListener("click", function () {
   modal.style.display = "block"; //show modal
   document.querySelector("body").style.overflow = "hidden"; //disable page scroll
 });
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none"; //close by click in any place
-    document.querySelector("body").style.overflow = "auto"; //page scroll
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    closeModal();
   }
-};
+});
 
 //get value from modal inputs
 
-let login = document.getElementById("login-text");
+const checkBtn = document.getElementById("login-check");
+const login = document.getElementById("login-text");
+const password = document.getElementById("login-password");
+const userName = document.getElementById("user-name");
+const buttonLogout = document.getElementById("button-logout");
+const checkbox = document.getElementById("checkbox");
 
-function getInputValue() {
-  // Selecting the input element and get its value
-  let inputValue = login.value;
-
-  alert(inputValue);
+function showUser(argument) {
+  userName.textContent = argument;
+  buttonLogout.classList.remove("button__hidden");
+  btn.classList.add("button__hidden");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const userLogin = localStorage.getItem("user");
+  const userLoginSession = sessionStorage.getItem("user");
+  const currentUser = userLogin || userLoginSession;
+  if (currentUser !== null) {
+    showUser(currentUser);
+  }
+});
+
+checkBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (login.value === "Константин" && password.value === "777") {
+    if (checkbox.checked) {
+      localStorage.setItem("user", login.value);
+    } else {
+      sessionStorage.setItem("user", login.value);
+    }
+    showUser(login.value);
+    closeModal();
+  } else {
+    alert("Incorrect login or password");
+  }
+});
+
+buttonLogout.addEventListener("click", function () {
+  login.value = "";
+  password.value = "";
+  checkbox.checked = false;
+  localStorage.removeItem("user");
+  sessionStorage.removeItem("user");
+  userName.textContent = "";
+  buttonLogout.classList.add("button__hidden");
+  btn.classList.remove("button__hidden");
+});
--- gulpfile.js
+++ gulpfile.js
@@ -39,8 +39,6 @@ const compileScript = () => {
   return gulp
     .src([
       // here add js libs
-      "app/libs/jquery-3.5.1.min.js",
-      "app/libs/swup.min.js",
       "app/js/script.js",
     ])
     .pipe(concat("scripts.min.js"))
